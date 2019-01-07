0abca0cfdc0e4c505cf15568093cabc444d1854b
andyjennings314 <andyjennings314@gmail.com>
2019-01-07 11:14:51 +0000

    Initialised API call

--- andygetest.js
+++ andygetest.js
@@ -1,1 +1,11 @@
-// JavaScript Document
+(function(){ 
+	var YTlink = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails%2Cstatus&maxResults=5&playlistId=UUTI5S0PqpgB0DbYgcgRU6QQ&key=AIzaSyCZ5jr4KJ-HebG-RH508ezGIvwzZMBYoJA";
+	
+	function populateYTContent(data){
+		alert(data.items);
+	}
+	
+	$.get( YTlink, function( data ) {
+	  populateYTContent(data);
+	});
+})();
