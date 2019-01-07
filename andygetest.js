d5badc09d2ad5f72ac1b4903d3930573bee0123a
andyjennings314 <andyjennings314@gmail.com>
2019-01-07 12:20:01 +0000

    Added alternating colour scheme, first bit of date formatting

--- andygetest.js
+++ andygetest.js
@@ -2,11 +2,13 @@
 	var YTlink = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails%2Cstatus&maxResults=5&playlistId=UUTI5S0PqpgB0DbYgcgRU6QQ&key=AIzaSyCZ5jr4KJ-HebG-RH508ezGIvwzZMBYoJA";
 	
 	function generateListEntry(listEntry) {
+		var videoDate = new Date(listEntry.publishedAt);
+		
 		var content="<div class='playlist-entry'>"
 		
 		content += "<a target='_blank' href='https://www.youtube.com/watch?v=" + listEntry.resourceId.videoId + "'><img src='" + listEntry.thumbnails.medium.url + "' /></a>";
 		content += "<h2><a target='_blank' href='https://www.youtube.com/watch?v=" + listEntry.resourceId.videoId + "'>" + listEntry.title + "</a></h2>";
-		content += "<p>" + listEntry.publishedAt + "</p>";
+		content += "<p>" + videoDate + "</p>";
 		content += "<p>" + listEntry.description + "</p>";
 		
 		content += "</div>";
