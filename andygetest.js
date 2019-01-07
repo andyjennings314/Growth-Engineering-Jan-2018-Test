0103aff80e2c393f0ebbdc14806f0f6351c99525
andyjennings314 <andyjennings314@gmail.com>
2019-01-07 11:36:34 +0000

    First draft of publishing playlist content

--- andygetest.js
+++ andygetest.js
@@ -1,8 +1,19 @@
 (function(){ 
 	var YTlink = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails%2Cstatus&maxResults=5&playlistId=UUTI5S0PqpgB0DbYgcgRU6QQ&key=AIzaSyCZ5jr4KJ-HebG-RH508ezGIvwzZMBYoJA";
 	
+	function generateListEntry(listEntry) {
+		var content="<div class='playlist-entry'>"
+		
+		content += "<h2>" + listEntry.snippet.title + "</h2>";
+		
+		content += "</div>";
+		return content;
+	}
+	
 	function populateYTContent(data){
-		alert(data.items);
+		data.items.forEach(function(listEntry){
+			$("#playlistHolder").append(generateListEntry(listEntry));
+		})
 	}
 	
 	$.get( YTlink, function( data ) {
